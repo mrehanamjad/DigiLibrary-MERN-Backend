f74f6a0c11f79e0eec3f0936611238f0006667c9
mrehanamjad <rehanamjad814@gmail.com>
2025-07-22 18:49:31 +0500

    Enhance book management: add deleteBook and getBookById routes; update publishABook and updateBook logic; integrate cover image deletion and validation.

--- backend/src/controllers/book.controller.ts
+++ backend/src/controllers/book.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { asyncHandler } from "../utils/asyncHandler";
 import { ApiError } from "../utils/ApiError";
 import { ApiResponse } from "../utils/ApiResponse";
-import { uploadOnImageKit } from "../utils/imagekit";
+import { deleteFileImagekit, uploadOnImageKit } from "../utils/imagekit";
 import { Book } from "../models/book.model";
 import { AuthRequest } from "../types/user.types";
 import { MatchStageI, SortStageI } from "../types/book.types";
@@ -18,7 +18,7 @@ const publishABook = asyncHandler(async (req: Request, res: Response) => {
     title,
     description,
     author,
-    price,
+    price = 0,
     category,
     tags = [],
     isPublished,
@@ -28,14 +28,19 @@ const publishABook = asyncHandler(async (req: Request, res: Response) => {
     !title ||
     !description ||
     author.length == 0 ||
-    price === undefined ||
     !category ||
     isPublished === undefined
   ) {
     throw new ApiError(400, "All fields are required");
   }
 
+ if (Number(price) < 0) {
+  throw new ApiError(400, "Invalid price. Price cannot be negative.");
+}
+
+
   const files = req.files as unknown as MulterFileFields;
+  // We use as unknown as MulterFileFields because req.files is of type any or some incompatible type, and TypeScript doesn’t allow direct casting from an unrelated type (like any[], File[], or Express.Multer.File[]) to MulterFileFields without first casting to unknown.
 
   const coverImageLocalPath = files.coverImage[0].path;
   const fileLocalPath = files.file[0].path;
@@ -88,96 +93,134 @@ const publishABook = asyncHandler(async (req: Request, res: Response) => {
     .json(new ApiResponse(201, newBook, "Book published successfully"));
 });
 
-const updateBook = asyncHandler(async (req: Request, res: Response) => {});
-const deleteBook = asyncHandler(async (req: Request, res: Response) => {});
+const updateBook = asyncHandler(async (req: Request, res: Response) => {
+  const { bookId } = req.params;
+  const _req = req as AuthRequest;
 
-// const getAllBooks = asyncHandler(async (req: Request, res: Response) => {
-//   const {
-//     page = 1,
-//     limit = 10,
-//     query = "",
-//     sortBy = "createdAt",
-//     sortType = "desc",
-//     author,
-//     category,
-//     isFree,
-//     userId,
-//   } = req.query;
+  const {
+    title,
+    description,
+    author,
+    price,
+    category,
+    tags = [],
+    isPublished,
+  } = req.body;
 
-//   const skipItems = (Number(page) - 1) * Number(limit);
-//   const limitItems = Number(limit);
+  const book = await Book.findById(bookId);
+  if (!book) {
+    throw new ApiError(404, "Book not found");
+  }
 
-//   // Match conditions for filters
-//   const matchStage: MatchStageI = {
-//     isPublished: true,
-//     ...(userId &&
-//       typeof userId === "string" && {
-//         userId: new mongoose.Types.ObjectId(userId),
-//       }),
-//     ...(category && typeof category === "string" && { category }),
-//     ...(author && typeof author === "string" && { author }),
-//     ...(typeof isFree === "string" && { isFree: isFree === "true" }),
-//   };
+  if (book.userId.toString() !== _req.user._id.toString()) {
+    throw new ApiError(
+      403,
+      "Unauthorized, you are not allowed to update this book"
+    );
+  }
 
-//   console.log("match stage", matchStage);
+  const updateFields = {
+    title,
+    description,
+    author,
+    price,
+    category,
+    tags,
+    isPublished,
+  };
 
-//   // Optional full-text search
-//   const searchStage =
-//     typeof query === "string" && query.trim().length > 0
-//       ? [{ $match: { $text: { $search: query.trim() } } }]
-//       : [];
+  for (const [key, value] of Object.entries(updateFields)) {
+    if (
+      typeof value !== "undefined" &&
+      JSON.stringify(book[key as keyof typeof book]) !== JSON.stringify(value)
+    ) {
+      (book[key as keyof typeof book] as any) = value;
+      if (key == "price") {
+        if (book.price < 0) throw new ApiError(400, "Price can not be negative");
+        book.isFree = book.price <= 0;
+      }
+    }
+  }
 
-//   // Sorting object
-//   const sortStage: SortStageI = {};
-//   sortStage[sortBy as string] = sortType === "desc" ? -1 : 1;
+  await book.save();
 
-//   const books = await Book.aggregate([
-//     ...searchStage,
-//     { $match: matchStage },
-//     { $sort: sortStage },
-//     { $skip: skipItems },
-//     { $limit: limitItems },
-//     {
-//       $lookup: {
-//         from: "users",
-//         localField: "userId",
-//         foreignField: "_id",
-//         as: "user",
-//       },
-//     },
-//     { $unwind: "$user" },
-//     {
-//       $project: {
-//         title: 1,
-//         description: 1,
-//         author: 1,
-//         price: 1,
-//         isFree: 1,
-//         category: 1,
-//         tags: 1,
-//         views: 1,
-//         downloads: 1,
-//         isPublished: 1,
-//         createdAt: 1,
-//         coverImage: 1,
-//         file: 1,
-//         user: {
-//           _id: "$user._id",
-//           username: "$user.username",
-//           fullName: "$user.fullName",
-//         },
-//       },
-//     },
-//   ]);
+  return res
+    .status(200)
+    .json(new ApiResponse(200, book, "Book updated successfully"));
+});
 
-//   if (!books) {
-//     throw new ApiError(404, "No books found");
-//   }
+const updateBookCoverImage = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { bookId } = req.params;
+    const _req = req as AuthRequest;
 
-//   return res
-//     .status(200)
-//     .json(new ApiResponse(200, books, "Books fetched successfully"));
-// });
+    const book = await Book.findById(bookId);
+    if (!book) {
+      throw new ApiError(404, "Book not found");
+    }
+
+    if (book.userId.toString() !== _req.user._id.toString()) {
+      throw new ApiError(403, "Unauthorized user");
+    }
+
+    const newCoverImageLocalPath = req.file?.path;
+    if (!newCoverImageLocalPath) {
+      throw new ApiError(400, "Cover image file not found");
+    }
+
+    const uploadedCoverImage = await uploadOnImageKit(
+      newCoverImageLocalPath,
+      req.file?.filename as string,
+      true
+    );
+    if (!uploadedCoverImage) {
+      throw new ApiError(500, "Error uploading cover image to ImageKit");
+    }
+
+    if (book.coverImage?.fileId) {
+      await deleteFileImagekit(book.coverImage.fileId);
+    }
+
+    book.coverImage = {
+      url: uploadedCoverImage.url,
+      fileId: uploadedCoverImage.fileId,
+    };
+
+    await book.save({ validateBeforeSave: false });
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, book, "Book cover image updated successfully")
+      );
+  }
+);
+
+const deleteBook = asyncHandler(async (req: Request, res: Response) => {
+  const { bookId } = req.params;
+  const _req = req as AuthRequest;
+
+  const book = await Book.findById(bookId);
+  if (!book) {
+    throw new ApiError(404, "Book not found");
+  }
+
+  if (book.userId.toString() !== _req.user._id.toString()) {
+    throw new ApiError(
+      403,
+      "Unauthorized, you are not allowed to delete this book"
+    );
+  }
+
+  await deleteFileImagekit(book.coverImage.fileId);
+  await deleteFileImagekit(book.file.fileId);
+
+  await book.deleteOne();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Book deleted successfully"));
+});
 
 const getAllBooks = asyncHandler(async (req: Request, res: Response) => {
   const {
@@ -249,6 +292,7 @@ const getAllBooks = asyncHandler(async (req: Request, res: Response) => {
           _id: "$user._id",
           username: "$user.username",
           fullName: "$user.fullName",
+          avatar: "$user.avatar",
         },
       },
     },
@@ -259,15 +303,15 @@ const getAllBooks = asyncHandler(async (req: Request, res: Response) => {
     page: Number(page),
     limit: Number(limit),
     customLabels: {
-      totalDocs: 'totalBooks',
-      docs: 'books',
-      limit: 'limit',
-      page: 'currentPage',
-      nextPage: 'nextPage',
-      prevPage: 'prevPage',
-      totalPages: 'totalPages',
-      pagingCounter: 'pagingCounter',
-      meta: 'pagination',
+      totalDocs: "totalBooks",
+      docs: "books",
+      limit: "limit",
+      page: "currentPage",
+      nextPage: "nextPage",
+      prevPage: "prevPage",
+      totalPages: "totalPages",
+      pagingCounter: "pagingCounter",
+      meta: "pagination",
     },
   };
 
@@ -286,9 +330,70 @@ const getAllBooks = asyncHandler(async (req: Request, res: Response) => {
     .json(new ApiResponse(200, result, "Books fetched successfully"));
 });
 
-const getBookById = asyncHandler(async (req: Request, res: Response) => {});
+const getBookById = asyncHandler(async (req: Request, res: Response) => {
+  const { bookId } = req.params;
 
-export { publishABook,getAllBooks };
+  if (!bookId) {
+    throw new ApiError(400, "Book ID is required");
+  }
+
+  const objectId = new mongoose.Types.ObjectId(bookId);
+
+  const book = await Book.aggregate([
+    { $match: { _id: objectId } },
+    {
+      $lookup: {
+        from: "users",
+        localField: "userId",
+        foreignField: "_id",
+        as: "user",
+      },
+    },
+    { $unwind: "$user" },
+    {
+      $project: {
+        title: 1,
+        description: 1,
+        author: 1,
+        price: 1,
+        isFree: 1,
+        category: 1,
+        tags: 1,
+        views: 1,
+        downloads: 1,
+        isPublished: 1,
+        createdAt: 1,
+        coverImage: 1,
+        file: 1,
+        user: {
+          _id: "$user._id",
+          username: "$user.username",
+          fullName: "$user.fullName",
+          avatar: "$user.avatar",
+        },
+      },
+    },
+  ]);
+
+  if (!book) {
+    throw new ApiError(404, "Book not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, book[0], "Book fetched successfully"));
+});
+
+
+
+export {
+  publishABook,
+  getAllBooks,
+  updateBook,
+  deleteBook,
+  updateBookCoverImage,
+  getBookById,
+};
 
 /*
 isFree = false // only paid
--- backend/src/routes/book.routes.ts
+++ backend/src/routes/book.routes.ts
@@ -1,17 +1,37 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware";
 import { upload } from "../middlewares/multer.middleware";
-import { getAllBooks, publishABook } from "../controllers/book.controller";
+import {
+  deleteBook,
+  getAllBooks,
+  getBookById,
+  publishABook,
+  updateBook,
+  updateBookCoverImage,
+} from "../controllers/book.controller";
 
 const router = Router();
 
-router.route("/").get(getAllBooks).post(
-  verifyJWT,
-  upload.fields([
-    { name: "file", maxCount: 1 },
-    { name: "coverImage", maxCount: 1, },
-  ]),
-  publishABook
-);
+router
+  .route("/")
+  .get(getAllBooks)
+  .post(
+    verifyJWT,
+    upload.fields([
+      { name: "file", maxCount: 1 },
+      { name: "coverImage", maxCount: 1 },
+    ]),
+    publishABook
+  );
+
+router
+  .route("/:bookId")
+  .get(getBookById)
+  .delete(verifyJWT, deleteBook)
+  .patch(verifyJWT, updateBook);
+
+router
+  .route("/:bookId/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateBookCoverImage);
 
 export default router;
